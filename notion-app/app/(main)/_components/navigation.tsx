92bdb1dc05c52479aac8f313e9e2da19181f2f31
Sahil-047 <sahil_gcse2022@msit.edu.in>
2025-02-21 19:24:58 +0530

    feat: add DocumentList component for sidebar navigation and implement loading skeletons

--- notion-app/app/(main)/_components/navigation.tsx
+++ notion-app/app/(main)/_components/navigation.tsx
@@ -6,16 +6,19 @@ import { usePathname } from "next/navigation";
 import { useRef, ElementRef, useState, useEffect, use } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { UserItem } from "./user-item";
-import { useMutation, useQuery } from "convex/react";
+import { useMutation, useQuery, } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { Item } from "./item";
 import { toast } from "sonner";
+import { DocumentList } from "./document-listt";
 
 
 export const Navigation = () => {
     const pathname = usePathname();
     const isMobile = useMediaQuery("(max-width: 768px)");
-    const documents = useQuery(api.documents.get);
+    const documents = useQuery(api.documents.getSidebar, {
+        parentDocument: undefined
+    });
     const create = useMutation(api.documents.create);
 
 
@@ -153,10 +156,7 @@ export const Navigation = () => {
                         icon={PlusCircle} />
                 </div>
                 <div className="mt-4">
-                    {documents?.map((document) => (
-                        <p key={document._id}>
-                            {document.title}</p>
-                    ))}
+                    <DocumentList />
                 </div>
                 <div
                     onMouseDown={handleMouseDown}
