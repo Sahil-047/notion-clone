151882896695c15899b8a728428a60cdf5b336c5
Sahil-047 <sahil_gcse2022@msit.edu.in>
2025-02-23 21:53:39 +0530

    feat: implement Input component and add ConfirmModal for alert dialogs

--- notion-app/app/(main)/_components/trash-box.tsx
+++ notion-app/app/(main)/_components/trash-box.tsx
@@ -1,9 +1,128 @@
 "use client";
 
-export const TrashBox = () => { 
+import { useParams, useRouter } from "next/navigation";
+import { useMutation, useQuery } from "convex/react";
+
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { toast } from "sonner";
+import { Spinner } from "@/components/spinner";
+import { Search, Trash, Undo } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { ConfirmModal } from "@/components/modals/confirm-modal";
+
+export const TrashBox = () => {
+    const router = useRouter();
+    const params = useParams();
+    const documents = useQuery(api.documents.getTrash, {});
+    const restore = useMutation(api.documents.restore);
+    const remove = useMutation(api.documents.remove);
+
+    const [search, setSearch] = useState("");
+
+    const filteredDocuments = documents?.filter((document) => {
+        return document.title.toLowerCase().includes(search.toLowerCase());
+    });
+
+    const onClick = (documentid: string) => {
+        router.push(`/document/${documentid}`);
+    };
+
+    const onRestore = (
+        event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+        documentid: Id<"documents">,
+    ) => {
+        event?.stopPropagation();
+        const promise = restore({ id: documentid });
+        toast.promise(promise, {
+            loading: "Restoring Note...",
+            success: "Note Restored",
+            error: "Failed to restore",
+        });
+    };
+
+    const onRemove = (
+        documentid: Id<"documents">,
+    ) => {
+        const event = window.event;
+        event?.stopPropagation();
+        const promise = remove({ id: documentid });
+        toast.promise(promise, {
+            loading: "Removing Note...",
+            success: "Note Removed",
+            error: "Failed to remove",
+        });
+    
+        if (params.documentid === documentid) {
+            router.push("/documents");
+        };
+    };
+
+    if (documents === undefined) {
+        return (
+            <div className="flex items-center justify-center h-full p-4">
+                <Spinner size="lg" />
+            </div>
+        )
+    }
+
     return (
-        <div>
-            Trash Box
+        <div className="text-sm">
+            <div className="flex items-center gap-x-1 p-2">
+                < Search className="h-4 w-4" />
+                < Input
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    className="h-7 px-2 focus-visible:ring-transparent bg-secondary"
+                    placeholder="Filter by page title"
+                />
+            </div>
+            <div className="mt-2 px-1 pb-1">
+                <div className="flex items-center gap-x-1 p-2 ">
+                    <Search className="h-4 w-4" />
+                    <Input
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                        className="h-7 px-2 focus-visible:ring-transparent bg-secondary"
+                        placeholder="Filter by page title"
+                    />
+                </div>
+            </div>
+            <div className="mt-2 px-1 pb-1">
+                <p className="hidden last:block text-xs text-muted-foreground pb-2 text-center">
+                    No notes in trash
+                </p>
+                {filteredDocuments?.map((document) => (
+                    <div
+                        key={document._id}
+                        className="flex items-center gap-x-2 p-2 cursor-pointer hover:bg-primary/10 justify-between"
+                        onClick={() => onClick(document._id)}
+                        role="button"
+                    >
+                        <span className="truncate pl-2">
+                            {document.title}
+                        </span>
+                        <div className="flex items-center">
+                            <div
+                                onClick={(e) => onRestore(e, document._id)}
+                                className="rounded-sm p-2 hover:bg-neutral-200"
+                            >
+                                <Undo className="h-4 w-4 text-muted-foreground" />
+                            </div>
+
+                            <ConfirmModal onConfirm={() => onRemove(document._id)}>
+                                <div
+                                    role="button"
+                                    className="rounded-sm p-2 hover:bg-neutral-200">
+                                    <Trash className="h-4 w-4 text-muted-foreground" />
+                                </div>
+                            </ConfirmModal>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
         </div>
     );
 }
--- notion-app/convex/documents.ts
+++ notion-app/convex/documents.ts
@@ -16,6 +16,7 @@ export const archive = mutation({
         }
         const userId = identity.subject;
         const existingDocument = await ctx.db.get(args.id);
+
         if (!existingDocument) {
             throw new Error("Document not found");
         }
@@ -31,26 +32,34 @@ export const archive = mutation({
                     q.eq("userId", userId).eq("parentDocument", documentId)
                 )
                 .collect();
-            for (const child of children) {
-                await ctx.db.patch(child._id, {
+
+
+            const updates = children.map((child) =>
+                ctx.db.patch(child._id, {
                     isArchived: true,
-                });
+                })
+            );
+
+
+            await Promise.all(updates);
+
+
+            for (const child of children) {
                 await recursiveArchive(child._id);
             }
+        };
 
 
+        const document = await ctx.db.patch(args.id, {
+            isArchived: true,
+        });
 
-            const document = await ctx.db.patch(args.id, {
-                isArchived: true,
-            });
 
-            recursiveArchive(args.id);
-            return document;
-            };
-    
-            await recursiveArchive(args.id);
-        },
-    });
+        await recursiveArchive(args.id);
+
+        return document;
+    },
+});
 
 export const getSidebar = query({
     args: {
@@ -100,3 +109,100 @@ export const create = mutation({
         return document;
     }
 });
+
+export const getTrash = query({
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+        if (!identity) {
+            throw new Error("Not authenticated");
+        }
+
+        const userId = identity.subject;
+
+
+        const document = await ctx.db
+            .query("documents")
+            .withIndex("by_user", (q) => q.eq("userId", userId))
+            .filter((q) => q.eq(q.field("isArchived"), true))
+            .order("desc")
+            .collect();
+
+        return document;
+    }
+})
+
+export const restore = mutation({
+    args: { id: v.id("documents") },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+        if (!identity) {
+            throw new Error("Not authenticated");
+        }
+
+        const userId = identity.subject;
+
+        const exisitingDocument = await ctx.db.get(args.id);
+        if (!exisitingDocument) {
+            throw new Error("Document not found");
+        }
+        if (exisitingDocument.userId !== userId) {
+            throw new Error("Not authorized");
+        }
+
+        const recursiveRestore = async (documentId: Id<"documents">) => {
+            const children = await ctx.db
+                .query("documents")
+                .withIndex("by_user_parent", (q) =>
+                    q.eq("userId", userId).eq("parentDocument", documentId)
+                )
+                .collect();
+            for (const child of children) {
+                await ctx.db.patch(child._id, {
+                    isArchived: false,
+                });
+                await recursiveRestore(child._id);
+            }
+        }
+
+        const options: Partial<Doc<"documents">> = {
+            isArchived: false
+        };
+
+        if (exisitingDocument.parentDocument) {
+            const parentDocument = await ctx.db.get(exisitingDocument.parentDocument);
+            if (parentDocument?.isArchived) {
+                options.parentDocument = undefined;
+            }
+        }
+        const document = await ctx.db.patch(args.id, options);
+
+        recursiveRestore(args.id);
+
+        return document;
+    }
+})
+
+
+export const remove = mutation({
+    args: { id: v.id("documents") },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+        if (!identity) {
+            throw new Error("Not authenticated");
+        }
+
+        const userId = identity.subject;
+
+
+        const exisitingDocument = await ctx.db.get(args.id);
+        if (!exisitingDocument) {
+            throw new Error("Document not found");
+        }
+        if (exisitingDocument.userId !== userId) {
+            throw new Error("Not authorized");
+        }
+
+        const document = await ctx.db.delete(args.id);
+        return document;
+    }
+})
