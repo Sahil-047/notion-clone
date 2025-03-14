1d5e74accb37b62d1407676ced9bdc3b9c2c6246
Sahil-047 <sahil_gcse2022@msit.edu.in>
2025-03-15 02:55:11 +0530

    oh shit

--- notion-app/components/spinner.tsx
+++ notion-app/components/spinner.tsx
@@ -20,7 +20,7 @@ const spinnerVariants = cva(
     },
 );
 
-interface SpinnerProps extends VariantProps<typeof spinnerVariants>{}
+interface SpinnerProps extends VariantProps<typeof spinnerVariants>{className?: string;}
 export const Spinner = ({
     size,
 }: SpinnerProps) => {
